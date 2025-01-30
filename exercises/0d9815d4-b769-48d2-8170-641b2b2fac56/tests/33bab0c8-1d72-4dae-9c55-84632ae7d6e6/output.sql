cf86fe59f5a68892771a6d5d8d127fd35fa71382
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-01-30 09:55:41 +0000

    created file exercises/0d9815d4-b769-48d2-8170-641b2b2fac56/tests/33bab0c8-1d72-4dae-9c55-84632ae7d6e6/output.sql

---
+++ exercises/0d9815d4-b769-48d2-8170-641b2b2fac56/tests/33bab0c8-1d72-4dae-9c55-84632ae7d6e6/output.sql
@@ -0,0 +1,3 @@
+tabla_origen|columna_origen |tabla_referenciada|columna_referenciada|regla_borrado|
+------------+---------------+------------------+--------------------+-------------+
+farmacias   |farmaceutico_id|farmaceuticos     |id                  |CASCADE      |
