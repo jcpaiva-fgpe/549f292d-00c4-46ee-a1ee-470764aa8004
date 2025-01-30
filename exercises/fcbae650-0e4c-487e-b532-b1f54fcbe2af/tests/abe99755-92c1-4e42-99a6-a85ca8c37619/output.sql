70fb3cd56eb230e95133221356b621dc501b262f
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-01-30 09:55:21 +0000

    created file exercises/fcbae650-0e4c-487e-b532-b1f54fcbe2af/tests/abe99755-92c1-4e42-99a6-a85ca8c37619/output.sql

---
+++ exercises/fcbae650-0e4c-487e-b532-b1f54fcbe2af/tests/abe99755-92c1-4e42-99a6-a85ca8c37619/output.sql
@@ -0,0 +1,4 @@
+column_name   |references_table|references_field|
+--------------+----------------+----------------+
+farmacia_id   |farmacias       |id              |
+medicamento_id|medicamentos    |id              |
