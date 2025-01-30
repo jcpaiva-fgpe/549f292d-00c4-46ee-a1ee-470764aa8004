8159a6597ddbcc6d8adafe91facd826f1fcb4412
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-01-30 09:55:49 +0000

    created file exercises/bf120e55-d511-4f83-a49c-ec2b9428d966/tests/469dbba1-fd57-43b8-a805-cac68a5f31ec/output.sql

---
+++ exercises/bf120e55-d511-4f83-a49c-ec2b9428d966/tests/469dbba1-fd57-43b8-a805-cac68a5f31ec/output.sql
@@ -0,0 +1,15 @@
+name                         |
+-----------------------------+
+Albania                      |
+Andorra                      |
+Bosnia and Herzegovina       |
+Croatia                      |
+Gibraltar                    |
+Greece                       |
+Holy See (Vatican City State)|
+Italy                        |
+Macedonia                    |
+Malta                        |
+Portugal                     |
+Slovenia                     |
+Yugoslavia                   |
